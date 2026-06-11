674a0b948d65056ee68d7e093af5cf9dc8161758
kushiganke612-cell <kushigank.e612@gmail.com>
2026-06-11 10:41:31 +0530

    Create Main.java

---
+++ assingment02/src/main/java/trackersystem/Main.java
@@ -0,0 +1,12 @@
+
+package trackersystem;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new TaskFrame().setVisible(true);
+        });
+    }
+}
