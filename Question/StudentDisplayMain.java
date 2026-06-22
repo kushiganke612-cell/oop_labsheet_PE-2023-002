dcdfcb78e5a674b5dd29a2b4b4f2706375eb2d2c
kushiganke612-cell <kushigank.e612@gmail.com>
2026-06-22 13:38:12 +0530

    Create StudentDisplayMain.java

---
+++ Question/StudentDisplayMain.java
@@ -0,0 +1,12 @@
+
+package Question;
+
+public class StudentDisplayMain {
+
+    public static void main(String[] args) {
+
+        StudentDAO dao = new StudentDAO();
+
+        dao.displayAllStudents();
+    }
+}
